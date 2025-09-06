f2ca472809c592ee7bb55dc3a4693540fe2f83c0
Chlmpanzy <pauligwe10@gmail.com>
2025-09-05 23:02:14 -0400

    feat: load more pages function

---
+++ app/controllers/concerns/pagination_helper.rb
@@ -0,0 +1,10 @@
+module PaginationHelper
+    extend ActiveSupport::Concern
+  
+    def paginate(collection, page:, per_page: 20)
+      paginated = collection.offset((page - 1) * per_page).limit(per_page)
+      has_more = collection.count > page * per_page
+  
+      [paginated, has_more]
+    end
+  end

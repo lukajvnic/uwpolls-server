1a7db711744a089dbe235303ec6050416f6beab2
Chlmpanzy <pauligwe10@gmail.com>
2025-09-08 18:35:27 -0400

    feat: added frontend origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'http://localhost:3001'  # Change so that its the frontend port
+      origins 'http://localhost:5173'  # Change so that its the frontend port
       resource '*',
         headers: :any,
         methods: [:get, :post, :put, :patch, :delete, :options, :head],

102fa6a899424192a56f0d49def0ac85075ab1cc
Chlmpanzy <pauligwe10@gmail.com>
2025-09-08 03:16:31 -0400

    fix: formatting issues which were causing CI to fail

--- app/controllers/concerns/pagination_helper.rb
+++ app/controllers/concerns/pagination_helper.rb
@@ -1,10 +1,8 @@
 module PaginationHelper
-    extend ActiveSupport::Concern
-  
+    extend ActiveSupport::Concern  
     def paginate(collection, page:, per_page: 20)
       paginated = collection.offset((page - 1) * per_page).limit(per_page)
-      has_more = collection.count > page * per_page
-  
-      [paginated, has_more]
+      has_more = collection.count > page * per_page  
+      [paginated,has_more]
     end
-  end
+end

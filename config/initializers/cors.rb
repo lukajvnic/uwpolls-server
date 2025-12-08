a6739a96f91b3c6c3666951a4b205a6f34c0cb2b
nelimalu <lukaj2501@gmail.com>
2025-12-07 22:59:25 -0500

    updated cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,8 +1,10 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:5000", "localhost:5173", "localhost:3000",
-            "127.0.0.1:5000", "127.0.0.1:5173", "127.0.0.1:3000",
-            "https://unmutative-countercurrently-leopoldo.ngrok-free.dev"
+    origins "localhost:5000", "localhost:5173", "localhost:3000", "localhost:5431",
+            "127.0.0.1:5000", "127.0.0.1:5173", "127.0.0.1:3000", "127.0.0.1:5431",
+            "https://unmutative-countercurrently-leopoldo.ngrok-free.dev",
+            "https://uwpolls-client.vercel.app",
+            "uwpolls-client-j22rje4rd-lukas-projects-e2762843.vercel.app"
     resource "*",
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
